ef01d840a28b5127abd6415092d2e00a5450c79b
artemyarik <86915417+artemyarik@users.noreply.github.com>
2023-05-10 23:47:51 +0300

    refactor booking script

--- script/tojson.js
+++ script/tojson.js
@@ -3,17 +3,17 @@ const selectroom = document.getElementById('rooms');
 const form = document.getElementById('booking-form'); 
 const clearbut = document.getElementById('clearbut');
 
-function opt(select, min, max){
+let opt = (select, min, max) =>  {   
     for(i=min; i<max; i++){
         var opt = document.createElement('option');
         opt.value = i;
         opt.innerHTML = i;
         select.appendChild(opt);
     }
-}
+};
+
 opt(selectfloor, 3, 28);
 opt(selectroom, 1, 11);
-
 
 form.addEventListener('submit', (e) => {  
   e.preventDefault()  
